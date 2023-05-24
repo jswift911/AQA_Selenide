a9019c4f2b37a288b43efe2aaa59f40b473a7d4a
Alex <jswift888@gmail.com>
2023-05-24 15:09:03 +0300

    add test with widgets

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,12 +1,14 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import com.google.common.collect.ImmutableMap;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -53,8 +55,11 @@ class ReservationCardTest {
 //    void setUp() {
 //        Configuration.browser = "edge";
 //        EdgeOptions options = new EdgeOptions();
-//        options.setCapability("goog:chromeOptions", ImmutableMap.of("w3c", false)); // отключение w3c протокола
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
 //        driver = new EdgeDriver(options);
+//        driver.get("http://localhost:9999");
 //    }
 
 //============================================================================
@@ -65,9 +70,11 @@ class ReservationCardTest {
         driver = null;
     }
 
+    // 1) Тест, без использования виджетов
+
     @Test
     void shouldReservationCard() {
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] .input__control").setValue("Калуга");
         $("[data-test-id='date'] .input__control").click();
@@ -75,6 +82,32 @@ class ReservationCardTest {
         $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
         String dateForReservation = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id='date'] .input__control").setValue(dateForReservation);
+        $("[data-test-id='name'] .input__control").setValue("Иван Петров-Иванов");
+        $("[data-test-id='phone'] [name='phone']").setValue("+79200000000");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(byText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofMillis(15000));
+    }
+
+    // 2) Тест, с использованием виджетов
+
+    @Test
+    void shouldReservationCardWithElements() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] .input__control").setValue("Ка");
+        $(By.xpath("//*[contains(@class, 'menu')]//*[contains(@class, 'menu-item') and contains(text(), 'Петропавловск-Камчатский')]")).click();
+        $(".calendar-input [type='button']").click();
+        String dateForReservation = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("d"));
+        SelenideElement day = $(By.xpath("//*[contains(@class, 'calendar-input__calendar-wrapper')]//*[contains(@class, 'calendar__layout')]//*[contains(@class, 'calendar__day') and contains(text(), " + dateForReservation + ")]"));
+        int weekForward = Integer.parseInt(LocalDate.now().format(DateTimeFormatter.ofPattern("d")));
+
+        if (Integer.parseInt(dateForReservation) < weekForward) {
+            $(By.xpath("//*[contains(@class, 'calendar__arrow_direction_right') and not(contains(@class, 'calendar__arrow_double'))]")).click();
+            day.click();
+        } else {
+            day.click();
+        }
+
         $("[data-test-id='name'] .input__control").setValue("Иван Петров-Иванов");
         $("[data-test-id='phone'] [name='phone']").setValue("+79200000000");
         $("[data-test-id='agreement'] .checkbox__box").click();
