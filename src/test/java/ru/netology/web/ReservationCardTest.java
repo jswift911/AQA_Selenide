d3a0c9daced6e50fd8a4e81230a11b77181829a4
Alex <jswift888@gmail.com>
2023-05-25 11:30:37 +0300

    add test with widgets

--- src/test/java/ru/netology/web/ReservationCardTest.java
+++ src/test/java/ru/netology/web/ReservationCardTest.java
@@ -2,19 +2,11 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
-import com.google.common.collect.ImmutableMap;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+
+
 import static com.codeborne.selenide.Selenide.open;
 
 import java.time.Duration;
@@ -27,47 +19,11 @@ import static com.codeborne.selenide.Selenide.*;
 
 class ReservationCardTest {
 
-    private  WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-//========= Драйвер для Microsoft Edge (не работает с Appveyor !!!) =========
-
-//    @BeforeAll
-//    static void setUpAll() {
-//        System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
-//        WebDriverManager.edgedriver().setup();
-//    }
-//
-//    @BeforeEach
-//    void setUp() {
-//        Configuration.browser = "edge";
-//        EdgeOptions options = new EdgeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
-//        driver = new EdgeDriver(options);
-//        driver.get("http://localhost:9999");
-//    }
-
-//============================================================================
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
+    public String generateDate(int plusDays, String pattern) {
+        if (plusDays == 0) {
+            return LocalDate.now().format(DateTimeFormatter.ofPattern(pattern));
+        }
+        return LocalDate.now().plusDays(plusDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     // 1) Тест, без использования виджетов
@@ -80,28 +36,29 @@ class ReservationCardTest {
         $("[data-test-id='date'] .input__control").click();
         $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
         $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
-        String dateForReservation = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String dateForReservation = generateDate(5, "dd.MM.yyyy");
         $("[data-test-id='date'] .input__control").setValue(dateForReservation);
         $("[data-test-id='name'] .input__control").setValue("Иван Петров-Иванов");
         $("[data-test-id='phone'] [name='phone']").setValue("+79200000000");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $(byText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofMillis(15000));
+        $(By.cssSelector(".notification__content")).shouldHave(exactText("Встреча успешно забронирована на " + dateForReservation), Duration.ofMillis(15000)).shouldBe(visible);
     }
 
     // 2) Тест, с использованием виджетов
 
     @Test
-    void shouldReservationCardWithElements() {
+    void shouldReservationCardWithWidgets() {
         open("http://localhost:9999");
         $("[data-test-id='city'] .input__control").setValue("Ка");
-        $(By.xpath("//*[contains(@class, 'menu')]//*[contains(@class, 'menu-item') and contains(text(), 'Петропавловск-Камчатский')]")).click();
+        $(By.xpath("//*[contains(@class, 'menu-item') and contains(text(), 'Петропавловск-Камчатский')]")).click();
         $(".calendar-input [type='button']").click();
-        String dateForReservation = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("d"));
-        SelenideElement day = $(By.xpath("//*[contains(@class, 'calendar-input__calendar-wrapper')]//*[contains(@class, 'calendar__layout')]//*[contains(@class, 'calendar__day') and contains(text(), " + dateForReservation + ")]"));
-        int currentDay = Integer.parseInt(LocalDate.now().format(DateTimeFormatter.ofPattern("d")));
+        int defaultDay = Integer.parseInt(generateDate(3,"d"));
+        int dayForReservation = Integer.parseInt(generateDate(10, "d"));
+        String fullDate = generateDate(10, "dd.MM.yyyy");
+        SelenideElement day = $(By.xpath("//*[contains(@class, 'calendar-input__calendar-wrapper')]//*[contains(@class, 'calendar__layout')]//*[contains(@class, 'calendar__day') and contains(text(), " + dayForReservation + ")]"));
 
-        if (Integer.parseInt(dateForReservation) < currentDay) {
+        if (dayForReservation < defaultDay) {
             $(By.xpath("//*[contains(@class, 'calendar__arrow_direction_right') and not(contains(@class, 'calendar__arrow_double'))]")).click();
             day.click();
         } else {
@@ -112,7 +69,7 @@ class ReservationCardTest {
         $("[data-test-id='phone'] [name='phone']").setValue("+79200000000");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $(byText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofMillis(15000));
+        $(By.cssSelector(".notification__content")).shouldHave(exactText("Встреча успешно забронирована на " + fullDate), Duration.ofMillis(15000)).shouldBe(visible);
     }
 }
 
